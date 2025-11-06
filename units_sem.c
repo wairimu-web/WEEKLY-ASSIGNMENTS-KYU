ce9968c058c8f2c43448823fea9ec9326c0fc46e
wairimu-web <sammynjogewairimu@gmail.com>
2025-11-06 15:38:36 +0300

    units offered

---
+++ units_sem.c
@@ -0,0 +1,25 @@
+// Description: This program displays a list of units offered in the current semester.
+/*
+name:        Samuel Njoroge Wairimu
+reg:         CT101/G/26428/25
+description: A C program that displays a list of units offered in the current semester.
+*/
+
+
+
+
+#include <stdio.h>
+
+int main() {
+    // Display heading
+    printf("Units Offered This Semester:\n");
+
+    // Display each unit on a new line
+    printf("1. Introduction to Programming\n");
+    printf("2. Data Structures\n");
+    printf("3. Computer Networks\n");
+    printf("4. Database Systems\n");
+    printf("5. Software Engineering\n");
+
+    return 0;
+}
